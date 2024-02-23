514f4d7180ff7a560082235f6d71d1206dbd29bb
Christy Babu <christybabu26@gmail.com>
2024-02-23 13:18:46 +0530

    fix(Profiles List): style fixes and added action buttons

--- src/features/profiles/pages/profiles-list/ProfilesList.tsx
+++ src/features/profiles/pages/profiles-list/ProfilesList.tsx
@@ -1,9 +1,11 @@
 import { useGetAllUserProfilesQuery } from '@features/profiles/queries/ProfileQuery';
 import { Profile } from '@types';
-import { List, Avatar, Divider } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { List, Avatar, Card, Button, Tooltip } from 'antd';
+import { UserOutlined, PlusOutlined, FilterOutlined } from '@ant-design/icons';
+import useTranslate from '@hooks/intl';
 
 const ProfilesList = () => {
+  const translate = useTranslate();
   const { data: profiles, isLoading } = useGetAllUserProfilesQuery();
 
   const renderProfileItem = (profileItem: Profile) => (
@@ -18,16 +20,46 @@ const ProfilesList = () => {
     </List.Item>
   );
 
+  const onCreateProfile = () => {};
+
+  const renderItemHeader = () => (
+    <div className="flex flex-row items-center justify-between">
+      <div className="text-h4 py-8 font-normal leading-7">
+        {translate('profiles.list.page_title')}
+      </div>
+      <div className="header-action-section flex flex-row items-center">
+        <Tooltip title={translate('profiles.list.filters')}>
+          <div className="mr-4 flex items-center justify-center">
+            <Button
+              type="default"
+              icon={<FilterOutlined />}
+              onClick={onCreateProfile}
+            />
+          </div>
+        </Tooltip>
+        <Tooltip title={translate('profiles.list.create_profile')}>
+          <div className="flex items-center justify-center">
+            <Button
+              type="default"
+              icon={<PlusOutlined />}
+              onClick={onCreateProfile}
+            />
+          </div>
+        </Tooltip>
+      </div>
+    </div>
+  );
+
   return (
-    <div className="profiles-list-wrapper size-full p-10">
-      <div className="text-h4 font-normal leading-7">Profiles</div>
-      <Divider className="my-2" />
-      <List
-        itemLayout="horizontal"
-        dataSource={profiles}
-        renderItem={renderProfileItem}
-        loading={isLoading}
-      />
+    <div className="profiles-list-wrapper size-full p-6">
+      <Card title={renderItemHeader()} className="h-full">
+        <List
+          itemLayout="horizontal"
+          dataSource={profiles}
+          renderItem={renderProfileItem}
+          loading={isLoading}
+        />
+      </Card>
     </div>
   );
 };
